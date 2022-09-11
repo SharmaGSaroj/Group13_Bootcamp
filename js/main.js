21c03f4bb12586c66e17239bc99465f145a19752
Bhavya-Fanshawe <bhavyathakkar396@gmail.com>
2022-09-11 17:14:00 -0400

    Final designer commit

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".menu");
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
 
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
+  function openService(All) {
+    var i;
+    var x = document.getElementsByClassName("All");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    document.getElementById(ServiceName).style.display = "block";
+  }
